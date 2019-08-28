1e311d5291c9340261ad702fe9c16c6dd71c6d77
Pendar Ghalamchi <pendar747@gmail.com>
2019-08-28 01:02:45 +0100

    build works

--- lib/uiFrame/index.tsx
+++ lib/uiFrame/index.tsx
@@ -1,5 +1,5 @@
-import ReactDom from 'react-dom';
-import React from 'react';
+import * as ReactDom from 'react-dom';
+import * as React from 'react';
 
 const Application = () => (
   <div>Hello World!!</div>
